4fcdff19859e2f163658701b21a6b799b2d76b48
Jose Lorenzo Moreno <jolorenzom@gmail.com>
2019-12-01 16:28:06 +0100

    Create structure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAaMlZj3tVuVhHZSjyzYaWfEQXW666ZM5g',
+    authDomain: 'crud-angular-firestore-app.firebaseapp.com',
+    databaseURL: 'https://crud-angular-firestore-app.firebaseio.com',
+    projectId: 'crud-angular-firestore-app',
+    storageBucket: 'crud-angular-firestore-app.appspot.com',
+    messagingSenderId: '102108470950',
+    appId: '1:102108470950:web:199cbd92139c7dbe8b5e67'
+  }
 };
 
 /*
