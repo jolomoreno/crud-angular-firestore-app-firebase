ac4c47d537566dc07baac251edefa5b2f73ff6b7
Jose Lorenzo Moreno <jolorenzom@gmail.com>
2019-12-01 18:28:35 +0100

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {environment} from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireModule} from '@angular/fire';
 import { EmployeesComponent } from './employees/employees.component';
-import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeService } from './shared/employee.service';
 import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
@@ -15,8 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent,
-    EmployeeComponent
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -3,6 +3,7 @@ import {Employee} from '../shared/employee.model';
 import {EmployeeService} from '../shared/employee.service';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {ToastrService} from 'ngx-toastr';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-employees',
@@ -11,12 +12,14 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class EmployeesComponent implements OnInit {
   public employees: Employee[];
+  public titlePage = 'EMP. Create';
 
   constructor(private readonly employeeService: EmployeeService,
               private readonly firestore: AngularFirestore,
               private readonly toastrService: ToastrService) { }
 
   ngOnInit() {
+    this.resetForm();
     this.employeeService.getEmployees().subscribe(
       response => {
         this.employees = response.map(employeesFirestoreData => {
@@ -29,12 +32,43 @@ export class EmployeesComponent implements OnInit {
     );
   }
 
+  resetForm(form?: NgForm) {
+    this.titlePage = 'EMP. Create';
+    if (form != null) {
+      form.resetForm();
+    }
+    this.employeeService.formData = {
+      id: null,
+      fullName: '',
+      position: '',
+      empCode: '',
+      mobile: ''
+    };
+  }
+
+  onSubmit(form: NgForm) {
+    this.titlePage = 'EMP. Create';
+    const title = (form.value.id == null) ? 'EMP. Create' : 'EMP. Edit';
+    const message = (form.value.id == null) ? 'Employee created successfully' : 'Employee edited successfully';
+    const data = Object.assign({}, form.value);
+    delete data.id;
+    if (form.value.id === null) {
+      this.firestore.collection('employees').add(data);
+    } else {
+      this.firestore.doc(`employees/${form.value.id}`).update(data);
+    }
+    this.resetForm(form);
+    this.toastrService.success(message, title);
+  }
+
   onEdit(employee: Employee) {
+    this.titlePage = 'EMP. Edit';
     this.employeeService.formData = Object.assign({}, employee);
   }
 
   onDelete(employeeId: string) {
     if (confirm('Are you sure to delete this record?')) {
+      this.titlePage = 'EMP. Create';
       this.firestore.doc(`employees/${employeeId}`).delete();
       this.toastrService.warning('Employee deleted successfully', 'EMP. Delete');
     }
